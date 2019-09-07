17fcbc675042ce67ad724605e4c1a561227beb24
Stephanie <almeidasmail@gmail.com>
2019-09-07 11:36:48 -0400

    Fix maps

--- screens/Home.js
+++ screens/Home.js
@@ -4,6 +4,7 @@ import {
   Text,
   TextInput,
   Button,
+  StyleSheet
 } from 'react-native';
 
 import { Stats } from './../components';
@@ -12,11 +13,7 @@ import Geocode from 'react-geocode';
 
 import RadioForm from 'react-native-simple-radio-button';
 
-export const getCurrentLocation = () => {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e));
-  });
-};
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const radio_props = [
   {label: 'Distance', value: 0 },
@@ -64,31 +61,74 @@ class Home extends React.Component {
   render() {
     const { navigate } = this.props.navigation;
     return(
-      <View>
-        <TextInput
-          placeholder="Enter starting point"
-          onChangeText={ (text) => this.setState({location: text})}/>
-        <RadioForm
-          radio_props={radio_props}
-          initial={0}
-          formHorizontal={true}
-          onPress = {(value) => {
-            if (value) {
-              this.setState({radio: "Time"})
-            } else {
-              this.setState({radio: "Distance"})
-            }
+      <View style = {styles.box}>
+        <MapView
+          provider = {PROVIDER_GOOGLE}
+          region={{
+            latitude: 39.951665,
+            longitude: -75.190424,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
           }}
+          style={styles.map}
         />
-        { this.onPress() }
-        <Button
-          onPress = {() => this.getAddr(navigate)}
-          title="Calculate">
-          Calculate
-        </Button>
+        <View style={styles.container}>
+          <TextInput
+            placeholder="Enter starting point"
+            onChangeText={ (text) => this.setState({location: text})}/>
+          <RadioForm
+            radio_props={radio_props}
+            initial={0}
+            formHorizontal={true}
+            radioStyle={{paddingTop: 20, paddingRight: 30}}
+            onPress = {(value) => {
+              if (value) {
+                this.setState({radio: "Time"})
+              } else {
+                this.setState({radio: "Distance"})
+              }
+            }}
+          />
+          { this.onPress() }
+          <Button
+            onPress = {() => this.getAddr(navigate)}
+            title="Calculate">
+            Calculate
+          </Button>
+        </View>
       </View>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  map: {
+    zIndex: -1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  container: {
+    zIndex: 10,
+    backgroundColor: 'white',
+    position: 'absolute',
+    alignSelf: 'stretch',
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  box: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  }
+});
 
 export default Home;
--- screens/Map.js
+++ screens/Map.js
@@ -2,31 +2,181 @@ import React, {Fragment} from 'react';
 import {
   View,
   Text,
-  Button
+  Button,
+  TouchableHighlight,
+  StyleSheet
 } from 'react-native';
+import MapView from 'react-native-maps';
+import Geojson from 'react-native-geojson';
 
-import { TripSelector, Stats } from './../components';
+import { Stats } from './../components';
+
 
 class Map extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      roundBG: "#90EE90",
+      onewayBG: "#F5F5F5",
+      forceRefresh: Math.floor(Math.random() * 100),
+      location: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: [
+                [-122.4324, 37.78825],
+                [-75.190429, 39.951804],
+              ],
+            }
+          }
+        ]
+      }
+    }
+  }
+
+  onPressRoundTrip = () => {
+    this.setState({
+      roundBG: "#90EE90",
+      onewayBG: "#F5F5F5",
+      location: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: [
+                [-122.4324, 37.78825],
+                [-75.190429, 39.951804],
+              ],
+            }
+          }
+        ]
+      }
+    });
+  }
+
+  onPressOneWay = () => {
+    this.setState({
+      roundBG: "#F5F5F5",
+      onewayBG: "#90EE90",
+      location: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: [
+                [-75.190429, 39.951804],
+                [-122.4324, 37.78825],
+              ],
+            }
+          }
+        ]
+      }
+    });
+    this.setState({
+      forceRefresh: Math.floor(Math.random() * 100)
+    });
+  }
 
   static navigationOptions = {
     header: null,
   };
 
+
   render() {
     const { navigate } = this.props.navigation;
+
+    mapRendered = () => {
+      return (
+        <MapView style={styles.map}
+          key={this.state.forceRefresh}
+          region={{
+            latitude: this.state.location.features[0].geometry.coordinates[0][1],
+            longitude: this.state.location.features[0].geometry.coordinates[0][0],
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+          <Geojson
+            geojson= {this.state.location} />
+        </MapView>
+      );
+    }
+
     return(
-      <View>
-        <Text>Map Screen</Text>
-        <Stats />
-        <TripSelector />
-        <Button
-          title="Start"
-          onPress = {() => navigate("Tracking")}
-        />
+      <View style={styles.box}>
+        { mapRendered() }
+        <View style={styles.layout}>
+          <View style={styles.container}>
+            <Stats />
+            <View>
+              <TouchableHighlight
+                onPress = {() => this.onPressRoundTrip()}
+                style = {{backgroundColor: this.state.roundBG, paddingTop: 20, paddingLeft: 20, paddingRight: 20, paddingBottom: 20, alignItems: "center"}}
+              >
+                <Text>Round Trip</Text>
+              </TouchableHighlight>
+            </View>
+            <View>
+              <TouchableHighlight
+                onPress = {() => this.onPressOneWay()}
+                style = {{backgroundColor: this.state.onewayBG, paddingTop: 20, paddingLeft: 20, paddingRight: 20, paddingBottom: 20, alignItems: "center"}}
+              >
+                <Text>One-Way with Transport</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+          <Button
+            title="Start"
+            onPress = {() => navigate("Tracking")}
+            style={{textAlign: "center", justifyContent: "center", alignItems: "center"}}
+          />
+        </View>
       </View>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  map: {
+    zIndex: -1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  container: {
+    zIndex: 10,
+    backgroundColor: 'white',
+    // alignSelf: 'stretch',
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  box: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  layout: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  }
+})
 
 export default Map;
